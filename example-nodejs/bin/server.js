8b347e1c3d5d8412b8726939e6e37e7d8f459280
Trygve Lie <github@trygve-lie.com>
2013-05-08 22:32:24 +0200

    Adjusted url to reflect the real url now running in prod

--- example-nodejs/bin/server.js
+++ example-nodejs/bin/server.js
@@ -14,7 +14,7 @@ var WebSocket = require('ws');
 var connection = function() {
 
     // Set up a WebSocket connection to the real time server
-    var client = new WebSocket('ws://realtime.chargelizer.com/api/v1/stream/realtime');
+    var client = new WebSocket('ws://realtime.chargelizer.com/api/v1/stream?apikey={private_apikey}');
 
 
     // Message emitted on connection to the stream api
